96516320347c3d32fab26f046b3961c7ef5f330c
Ivan Muliarchyk <ya-gans@mail.ru>
2020-04-06 16:39:10 +0300

    Throttle attack trigger

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -7,9 +7,12 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField]float timeBetweenAttacks = 1f;
         Transform target;
+        float timeSinceLastAttack = 0;
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
             if (target == null) return;
             if (!GetIsInRange())
             {
@@ -18,7 +21,16 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
+            }
+        }
+
+        private void AttackBehaviour()
+        {
+            if (timeSinceLastAttack > timeBetweenAttacks) 
+            {
                 GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
             }
         }
 
