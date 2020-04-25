68f8f772193f5c6eadcd159c4fea0a36ea4d860e
Ivan Muliarchyk <ya-gans@mail.ru>
2020-04-25 12:17:52 +0300

    Implement health bar

--- Assets/Scripts/Attributes/Health.cs
+++ Assets/Scripts/Attributes/Health.cs
@@ -4,12 +4,19 @@ using RPG.Core;
 using RPG.Saving;
 using RPG.Stats;
 using UnityEngine;
+using UnityEngine.Events;
 
-namespace RPG.Resources
+namespace RPG.Attributes
 {
     public class Health : MonoBehaviour, ISaveable
     {
         [SerializeField] float regenerationPercentage = 70;
+        [SerializeField] TakeDamageEvent takeDamage;
+
+        [System.Serializable]
+        public class TakeDamageEvent : UnityEvent<float>
+        {
+        }
 
         LazyValue<float> healthPoints;
 
@@ -47,10 +54,15 @@ namespace RPG.Resources
             print(gameObject.name + " took damage: " + damage);
 
             healthPoints.value = Mathf.Max(healthPoints.value - damage, 0);
+            
             if(healthPoints.value == 0)
             {
                 Die();
                 AwardExperience(instigator);
+            } 
+            else
+            {
+                takeDamage.Invoke(damage);
             }
         }
 
@@ -66,7 +78,12 @@ namespace RPG.Resources
 
         public float GetPercentage()
         {
-            return 100 * (healthPoints.value / GetComponent<BaseStats>().GetStat(Stat.Health));
+            return 100 * GetFraction();
+        }
+
+        public float GetFraction()
+        {
+            return healthPoints.value / GetComponent<BaseStats>().GetStat(Stat.Health);
         }
 
         private void Die()
